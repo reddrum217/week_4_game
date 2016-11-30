b9add010a2f688cca30e45f5c91574e6204cb5dc
Daniel Vreeland <danielvreeland@Daniels-MBP.home>
2016-11-30 01:18:31 -0500

    Added sections

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+// 1. Place a value on the attack and health on all the characters.
+// 2. Make an arrray for the good characters.
+// 3. Make a seaparate arrary for the bad characters.
+// 4. Allow the player to choose his character from either the good list or the bad list.
+// 5. When character is chosen position the players choice in the top center of the screen.
+// 6. Disable the rest of the characters from the list of the players choice.
+// 7. Prompt the user to choose an enemy to attack from the opposite side of the screen.
+// 8. After user chooses place that character in the lower div in the middle of the screen.
+// 9. Create an attack button.
+// 10. Prompt the user to attack his opponent by hitting the attack button. 
+// 11. Play light saber sounds each time the attack button is hit.
+// 12. Each time the opponent is looses health make the picture light up and shake.
+// 13. Make a function that increases your attack power every time the user hits the attack button.
+// 14. Make a function that decreases the enemy's health if the attack power is lower than the users.
+// 15. When the player loses make the opponent disapper and alert that the play has lost.
+// 16. Play a sound clip to describe whether the player lost or won.
+// 17. Play music in the 
+
+
+$('#good').on('click', function(){
+	alert("hi");
+});
+
